be3b5622e72cc75a9c1cc07343c384a67dc48c77
shellikerishukla-04dot <shellikerishukla@gmail.com>
2025-11-15 23:08:53 +0530

    save to local

--- status.py
+++ status.py
@@ -1,7 +1,8 @@
 
 import sys
 
-
+# LOCAL BRANCH FUNCTIONALITY:
+# Sum + Average + Maximum + Minimum
 
 if len(sys.argv) > 1:
     scores = sys.argv[1:]  # Read from system arguments
@@ -13,8 +14,5 @@ else:
 # Convert using eval() for calculations
 total = sum(eval(x) for x in scores)
 average = total / len(scores)
-
-print("Sum of scores =", total)
-print("Average of scores =", average)
-
-
+maximum = max(scores, key=eval)
+minimum = min(scores, key=eval)
